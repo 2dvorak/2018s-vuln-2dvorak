4174dc169f7c395113c4cd709b0ce0e9539898fb
zanywhale <zanywhale@gmail.com>
2018-03-19 20:11:40 +0900

    Fix typo

--- onionMessenger/message.cpp
+++ onionMessenger/message.cpp
@@ -1,13 +1,11 @@
 
-#incldue "common.h"
+#include "common.h"
 #include "message.h"
 using namespace newmsg;
 
 Message::Message(){}
 
 Message::~Message(){}
-
-string Message::ParseToJson(){}
 
 void Message::SendMessage(){}
 
