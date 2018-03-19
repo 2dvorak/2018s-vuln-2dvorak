fe1c0be9cde74525a45d6eb2c7b8bd3c1cebd62c
zanywhale <zanywhale@gmail.com>
2018-03-19 17:17:36 +0900

    Add keymanager class

---
+++ onionMessenger/keymanager.cpp
@@ -0,0 +1,9 @@
+
+#include "common.h"
+#include "keymanager.h"
+
+Keymanager::Keymanager(){}
+
+Keymanager::~Keymanager(){}
+
+
--- onionMessenger/keymanager.h
+++ onionMessenger/keymanager.h
@@ -1,4 +1,17 @@
 #ifndef KEYMANAGER_H
 #define KEYMANAGER_H
 
+#include "common.h"
+using namespace std;
+
+namespace newkey{
+    class Keymanager{
+    public:
+        Keymanager();
+        virtual ~Keymanager();
+    private:
+    };
+}
+
+
 #endif // KEYMANAGER_H
