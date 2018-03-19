f886a70aec2de50288bf9df3746f1fce4ec2c7c9
zanywhale <zanywhale@gmail.com>
2018-03-19 17:20:27 +0900

    Update main.cpp for testing

--- onionMessenger/main.cpp
+++ onionMessenger/main.cpp
@@ -1,11 +1,25 @@
-#include <iostream>
+#include "common.h"
 #include "message.h"
+#include "newproto.h"
+#include "pgpcrypto.h"
 
 using namespace std;
 using namespace newmsg;
+using namespace newpro;
+using namespace newcrypto;
+using namespace newkey;
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    string test;
+    if( argc != 2 ){
+        cout << "Usage: ./onionMessenger [Private_Key]" << endl;
+    }
+    else{
+        cout << "Enter passphrase: ";
+        cin >> test;
+    }
+    cout << argv[2] << endl;
+    cout << test << endl;
     return 0;
 }
