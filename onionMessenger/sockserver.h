d5c3eb40d2a62d97ccc61c08ade130f98977e417
zanywhale <zanywhale@gmail.com>
2018-03-19 21:54:13 +0900

    Add new file sockserver.h

--- onionMessenger/sockserver.h
+++ onionMessenger/sockserver.h
@@ -1,4 +1,17 @@
 #ifndef SOCKSERVER_H
 #define SOCKSERVER_H
 
+#include "common.h"
+
+namespace socksv{
+    class Sockserver{
+    public:
+        Sockserver();
+        virtual ~Sockserver();
+        std::thread KeyServer();
+        std::thread MessageServer();
+    private:
+    };
+}
+
 #endif // SOCKSERVER_H
