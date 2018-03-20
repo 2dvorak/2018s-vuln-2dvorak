313b6a95a2a9278a5bf65f5262f9ab0bf96ce478
Seungyeop Lee <2dvorak@kaist.ac.kr>
2018-03-20 19:32:27 +0900

    Add socket logic to creatSocket in SockServer
    This codes were from
    http://www.bogotobogo.com/cplusplus/sockets_server_client.php

--- onionMessenger/sockserver.cpp
+++ onionMessenger/sockserver.cpp
@@ -42,6 +42,47 @@ namespace socksv{
         // if not, ping to that node and check if valid node?
         servAddr.sin_addr.s_addr = INADDR_ANY;
 
+        // bind(int fd, struct sockaddr *local_addr, socklen_t addr_length)
+        // bind socket to current IP address on portNum
+        if (bind(sockFd, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0) {
+            error("ERROR on binding");
+            return -1;
+        }
+
+        // listen() function put all new connections into backlog queue until accept()
+        // !!!!!!!!!!!!!!!! SOMAXCONN ok?
+        listen(sockfd, SOMAXCONN);
+
+        // ? what for..?
+        cliLen = sizeof(cliAddr);
+
+        // accept(int s, struct sockaddr *addr, socklen_t *addrlen);
+        // accept() function will write the connecting client's info
+        // into address structure then returns a new socket file descriptor
+        // for the accepted connection.
+        newSockFd = accept(sockFd, (struct sockaddr *) &cliAddr, &cliLen);
+        if (newSockFd < 0) {
+            error("ERROR on accept");
+            return -1;
+        }
+
+        printf("server: got connection from %s port %d\n", inet_ntoa(cliAddr.sin_addr), ntohs(cliAddr.sin_port));
+
+
+        // send 13 byte to new socket
+        send(newSockFd, "Hello, world!\n", 13, 0);
+
+        bzero(buffer,256);
+
+        n = read(newSockFd,buffer,255);
+        if (n < 0) {
+            error("ERROR reading from socket");
+            return -1;
+        }
+        printf("Here is the message: %s\n",buffer);
+
+        close(newSockFd);
+        close(sockFd);
         return 0;
     }
 
