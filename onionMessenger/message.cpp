fa0f36edb913f6abc4993bf8f1520531957d1b9f
zanywhale <zanywhale@gmail.com>
2018-03-24 16:15:33 +0900

    Update All of the sendkey

--- onionMessenger/message.cpp
+++ onionMessenger/message.cpp
@@ -6,34 +6,35 @@ namespace newmsg{
 
     Message::Message(){}
 
-    Message::Message(int id, bool bullian, string IP, string content) {
+    Message::Message(int id, bool bullian, string ip, string content) {
         this->id = id;
         this->bullian = bullian;
-        this->IP = IP;
+        this->ip = ip;
         this->content = content;
         this->jason["id"] = id;
         this->jason["bullian"] = bullian;
-        this->jason["IP"] = IP;
+        this->jason["ip"] = ip;
         this->jason["content"] = content;
     }
 
     Message::Message(json jason) {
         this->jason["id"] = jason["id"];
         this->jason["bullian"] = jason["bullian"];
-        this->jason["IP"] = jason["IP"];
+        this->jason["ip"] = jason["ip"];
         this->jason["content"] = jason["content"];
         this->id = jason["id"];
         this->bullian = jason["bullian"];
-        this->IP = jason["IP"];
+        this->ip = jason["ip"];
         this->content = jason["content"];
     }
 
-    Message::Message(string jsonStr) {
-        this->jason = json::parse(jsonStr);
-        this->id = this->jason["id"];
-        this->bullian = this->jason["bullian"];
-        this->IP = this->jason["IP"];
-        this->content = this->jason["content"];
+    // KEY
+    Message::Message(int id, int loginStatus, string ip, string githubID, string pubkey){
+        this->jason["id"] = id;
+        this->jason["bullian"] = loginStatus;
+        this->jason["ip"] = ip;
+        this->jason["githubID"] = githubID;
+        this->jason["pubkey"] = pubkey;
     }
 
     Message::~Message(){}
@@ -47,7 +48,7 @@ namespace newmsg{
     }
 
     string Message::getIP() {
-        return this->IP;
+        return this->ip;
     }
 
     string Message::getContent() {
@@ -62,12 +63,12 @@ namespace newmsg{
         this->id = id;
     }
 
-    void Message::setBullian(bool bullian) {
-        this->bullian = bullian;
+    void Message::setBullian(int bullian) {
+        this->jason["bullian"] = bullian;
     }
 
-    void Message::setIP(string IP) {
-        this->IP = IP;
+    void Message::setIP(string ip) {
+        this->jason["ip"] = ip;
     }
 
     void Message::setContent(string content) {
@@ -77,7 +78,7 @@ namespace newmsg{
     void Message::setJason(json) {
         this->jason["id"] = jason["id"];
         this->jason["bullian"] = jason["bullian"];
-        this->jason["IP"] = jason["IP"];
+        this->jason["ip"] = jason["ip"];
         this->jason["content"] = jason["content"];
     }
 
@@ -85,13 +86,22 @@ namespace newmsg{
         qSendMsg.push(this->jason.dump());
     }
 
+    void Message::SendKey(){
+        qSendMsg.push(this->jason.dump());
+    }
+
+    void Message::CheckMessage(){
+        cout << this->jason.dump();
+    }
+
     void Message::RecvMessage(){
         string str(qRecvMsg.front());
         qRecvMsg.pop();
+
         this->jason = json::parse(str);
         this->id = this->jason["id"];
         this->bullian = this->jason["bullian"];
-        this->IP = this->jason["IP"];
+        this->ip = this->jason["ip"];
         this->content = this->jason["content"];
     }
 
--- onionMessenger/message.h
+++ onionMessenger/message.h
@@ -9,27 +9,31 @@ namespace newmsg{
         Message();
         Message(int, bool, string, string);
         Message(json);
-        Message(string);
+        // for key
+        Message(int id, int loginStatus, string IP, string githubID, string pubkey);
         virtual ~Message();
         // Socket Client
         void SendMessage();
         void RecvMessage();
+        void SendKey();
+        void RecvKey();
         int getID();
         bool getBullian();
         string getIP();
         string getContent();
         json getJason();
         void setID(int);
-        void setBullian(bool);
+        void setBullian(int);
         void setIP(string);
         void setContent(string);
         void setJason(json);
+        void CheckMessage();
     private:
         // We have to make rules(Protocol)
         // +a // string attachment;
         int id;
-        bool bullian;
-        string IP;
+        int bullian;
+        string ip;
         string content;
         json jason;
     };
