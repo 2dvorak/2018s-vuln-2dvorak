a39a2491c247cfd48a3552fded0cd71437c8514c
zanywhale <zanywhale@gmail.com>
2018-03-25 23:25:27 +0900

    Set format PGPManager format

--- onionMessenger/pgpmanager.h
+++ onionMessenger/pgpmanager.h
@@ -2,19 +2,23 @@
 #define PGPMANAGER_H
 
 #include <iostream>
+#include <gpgme.h>
 using namespace std;
 
 namespace PGPCrypt{
-    class PGPManager;
-
+    
     class PGPManager{
     public:
         PGPManager();
         virtual ~PGPManager();
+        void SetPubKey(string a_pubkey);
+        void SetPriKey(string a_privkey);
         bool KeyValidation(string);
-        string Enc();
-        string Dec();
+        string Enc(string input_plain);
+        string Dec(string input_cipher);
     private:
+        string privKey;
+        string pubKey;
     };
 }
 
